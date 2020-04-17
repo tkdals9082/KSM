c473d9a4df203fb8875e890cc9aa16676627fc3c
김상민 <smini99@netblue.co.kr>
2020-04-17 17:59:23 +0900

    [ADD] ButtonDropdown

---
+++ Assets/KSM/UIExtensions/ButtonDropdown/Example/Scripts/Listener.cs
@@ -0,0 +1,31 @@
+﻿namespace KSM.UI.Example
+{
+    using UnityEngine;
+    using UnityEngine.UI;
+
+    public class Listener : MonoBehaviour
+    {
+        public ButtonDropdown buttonDropdown;
+
+        private void OnEnable()
+        {
+            buttonDropdown.OnFinishCreateList.AddListener(AddListenerForButtons);
+        }
+        
+        private void AddListenerForButtons()
+        {
+            int cnt = buttonDropdown.options.Count;
+
+            for(int i = 0; i < cnt; ++i)
+            {
+                int j = i;
+                buttonDropdown.buttons[i].onClick.AddListener(() => Debug.Log("Button " + j + " is clicked!"));
+            }
+        }
+
+        private void OnDisable()
+        {
+            buttonDropdown.OnFinishCreateList.RemoveListener(AddListenerForButtons);
+        }
+    }
+}
