4b180679fb9fafad1954c4abc3a1d45e7777cb34
김상민 <smini99@netblue.co.kr>
2020-06-22 16:28:08 +0900

    [ADD] 1. Simple example of Unity Animation

---
+++ Assets/KSM/UnityTest/Animation/Example/AnimController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimController : MonoBehaviour
+{
+    public Animator anim;
+
+    bool zIsPressed => Input.GetKey(KeyCode.Z);
+    bool xIsPressed => Input.GetKey(KeyCode.X);
+    bool yIsPressed => Input.GetKey(KeyCode.Y);
+
+    private void Update()
+    {
+        anim.SetBool("isXPressed", xIsPressed);
+        anim.SetBool("isYPressed", yIsPressed);
+        anim.SetBool("isZPressed", zIsPressed);
+    }
+}
